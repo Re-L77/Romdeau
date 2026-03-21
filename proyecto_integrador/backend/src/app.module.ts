2ac55d16b5191b1021a06453ccf85caf0e95be7d
Re-L77 <124051537@upq.edu.mx>
2026-03-21 11:40:13 -0600

    feat: add Prisma module and service, and integrate into AppModule

--- proyecto_integrador/backend/src/app.module.ts
+++ proyecto_integrador/backend/src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 function validateEnv(config: Record<string, unknown>) {
   const requiredVars = [
@@ -42,8 +44,9 @@ function validateEnv(config: Record<string, unknown>) {
       validate: validateEnv,
     }),
     AuthModule,
+    PrismaModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
